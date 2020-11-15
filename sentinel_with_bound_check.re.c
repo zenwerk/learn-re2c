d774f3127b2599e505122a61a6ef54d5106a6d6c
zenwerk <zenwerk@gmail.com>
2020-11-16 03:26:31 +0900

    init

---
+++ sentinel_with_bound_check.re.c
@@ -0,0 +1,31 @@
+// re2c $INPUT -o $OUTPUT 
+#include <assert.h>
+
+// expect a null-terminated string
+static int lex(const char *str, unsigned int len)
+{
+    const char *YYCURSOR = str, *YYLIMIT = str + len, *YYMARKER;
+    int count = 0;
+
+loop:
+    /*!re2c
+    re2c:define:YYCTYPE = char;
+    re2c:yyfill:enable = 0;
+    re2c:eof = 0;
+
+    *        { return -1; }
+    $        { return count; }
+    [a-z]+   { ++count; goto loop; }
+    [ ]+     { goto loop; }
+
+    */
+}
+
+#define TEST(s, r) assert(lex(s, sizeof(s) - 1) == r)
+int main()
+{
+    TEST("", 0);
+    TEST("one two three", 3);
+    TEST("f0ur", -1);
+    return 0;
+}
